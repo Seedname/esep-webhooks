294abe09603197e3011c238b8dce14acc6161033
Julian Dominguez <45999377+Seedname@users.noreply.github.com>
2025-04-11 22:59:59 -0400

    add gitignore

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-from botocore.vendored import requests
-def handler(event, context):
-    return "Test"
+import urllib3
+import json
+import os
+
+
+def lambda_handler(event, context):
+    if "issue" not in event:
+        return
+
+    if "html_url" not in event["issue"]:
+        return
+
+    session = urllib3.PoolManager()
+
+    payload = {
+        "text": f"Issue Created {event["issue"]["html_url"]}"
+    }
+
+    headers = {"Content-Type": "application/json"}
+
+    result = session.request('POST', os.environ["SLACK_URL"], headers=headers, body=json.dumps(payload))
+
+    return result.read()
